645104e7a052e492c8a03b2f76924ec5a7c2d2e3
Rick Koch <n1gp@hotmail.com>
2026-01-03 22:58:22 -0500

    Remove build artifacts.

--- ka9q_hpsdr.d
+++
@@ -1,4 +0,0 @@
-ka9q_hpsdr.o: ka9q_hpsdr.c ka9q_hpsdr.h ALT_SRC/status.h ALT_SRC/radio.h \
- ALT_SRC/config.h ALT_SRC/multicast.h ALT_SRC/rtp.h ALT_SRC/osc.h \
- ALT_SRC/status.h ALT_SRC/filter.h ALT_SRC/misc.h ALT_SRC/iir.h \
- ALT_SRC/conf.h
